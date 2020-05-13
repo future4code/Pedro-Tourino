8b1112fa72a57ba63a97549e5d45afedd45583d2
Pedro <ntourinocp@gmail.com>
2020-05-12 21:38:03 -0300

    src/Introdução a Componentes e Props

--- semana5/labedin/src/App.js
+++ semana5/labedin/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import CardGrande from "./components/CardGrande/CardGrande";
+import CardPequeno from "./components/CardPequeno/CardPequeno";
 import ImagemButton from "./components/ImagemButton/ImagemButton";
 
 function App() {
   return (
     <div className="App">
-      <div className="page-section-container">
+      <div className="page-section-container mb-0">
         <h2>Dados pessoais</h2>
         <CardGrande
           imagem="https://lh3.googleusercontent.com/8e6FSN3ow0fVfVZBFDZcvDLexNQ3MCYLNo3D5q2CJj0G_M69sSqZ2qrOO1LZpPor5ayu=s85"
@@ -17,6 +18,26 @@ function App() {
         <ImagemButton
           imagem="https://image.flaticon.com/icons/png/512/117/117472.png"
           texto="Ver mais"
+        />
+      </div>
+
+      <div className="page-section-container mt-0">
+        <CardPequeno
+          imagem="https://campcursos.com.br/wp-content/uploads/2015/09/contato-telefone.png"
+          nome="Telefone:"
+          descricao="(31) 99111-1111"
+        />
+
+        <CardPequeno
+          imagem="https://qifinanceiro.com.br/wp-content/uploads/2014/06/contact-email.png"
+          nome="Email:"
+          descricao="pedro@gmail.com"
+        />
+
+        <CardPequeno
+          imagem="https://cdn.iconscout.com/icon/free/png-256/navigate-find-pin-location-search-gps-home-5-29678.png"
+          nome="Endereço:"
+          descricao="Rua Afonso Camargo, 805, Santana, Belo Horizonte - MG, CEP: 30300-300"
         />
       </div>
 
@@ -32,6 +53,21 @@ function App() {
           imagem="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRM5KjDlaQXqGy-NhpTWe_NdJYS7t66f0B2mjvWNsYHGaZDONdZ&usqp=CAU"
           nome="Pixelz Informática"
           descricao="Técnico em Informática"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Formação acadêmica</h2>
+        <CardGrande
+          imagem="https://images.typeform.com/images/ZutsHDtXF7XR/image/default"
+          nome="Labenu_"
+          descricao="Desenvolvedor Web Full-Stack"
+        />
+
+        <CardGrande
+          imagem="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR0S7tqe6dlUouysgorrTqM7PAy24HlrYo5iLJCNpKx0owIavXH&usqp=CAU"
+          nome="Utramig"
+          descricao="Técnico em Tecnologia da Informação (Foco em Desenvolvimento)"
         />
       </div>
 
--- semana5/labedin/src/components/CardPequeno/CardPequeno.js
+++ semana5/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,1 +1,16 @@
-import 
+import React from "react";
+import "./CardPequeno.css";
+
+function CardPequeno(props) {
+  return (
+    <div className="smallcard-container">
+      <img src={props.imagem} />
+      <div>
+        <span>{props.nome}</span>
+        <p>{props.descricao}</p>
+      </div>
+    </div>
+  );
+}
+
+export default CardPequeno;
