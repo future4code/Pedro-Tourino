e704c7ae73183a862daf0bd35605de71edf2d791
Pedro <ntourinocp@gmail.com>
2020-05-14 15:16:42 -0300

    Componentes de classe e Estado

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -7,9 +7,19 @@ class App extends React.Component {
     return (
       <div className={"app-container"}>
         <Post
-          nomeUsuario={"paulinha"}
-          fotoUsuario={"https://picsum.photos/50/50"}
-          fotoPost={"https://picsum.photos/200/150"}
+          nomeUsuario={"soter"}
+          fotoUsuario={"https://picsum.photos/50/50?a=1"}
+          fotoPost={"https://picsum.photos/200/150?a=1"}
+        />
+        <Post
+          nomeUsuario={"darvas"}
+          fotoUsuario={"https://picsum.photos/50/50?a=2"}
+          fotoPost={"https://picsum.photos/200/150?a=2"}
+        />
+        <Post
+          nomeUsuario={"severo"}
+          fotoUsuario={"https://picsum.photos/50/50?a=3"}
+          fotoPost={"https://picsum.photos/200/150?a=3"}
         />
       </div>
     );
