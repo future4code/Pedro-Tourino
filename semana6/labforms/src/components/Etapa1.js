6c03e0920e8e488307fa4cb3200facbec66aa9de
Pedro <ntourinocp@gmail.com>
2020-05-19 08:21:43 -0300

    Renderização condicional

--- semana6/labforms/src/components/Etapa1.js
+++ semana6/labforms/src/components/Etapa1.js
@@ -1,34 +1,75 @@
 import React from "react";
 import styled from "styled-components";
 
-const StageContainer = styled.div`
+const FirstStageContainer = styled.div`
+  width: 800px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
   h1 {
     text-transform: uppercase;
   }
 
-  h2 {
-    font-weight: 500;
+  label {
+    font-size: xx-large;
+    font-weight: 400;
+  }
+
+  input {
+    margin: 20px;
+  }
+
+  select {
+    margin: 20px;
   }
 `;
 
 export class Etapa1 extends React.Component {
+  state = {
+    name: "",
+    age: 0,
+    email: "",
+    schooling: "",
+  };
+
   render() {
     return (
-      <StageContainer>
+      <FirstStageContainer>
         <h1>etapa 1 - dados gerais</h1>
-        <h2>1. Qual o seu nome?</h2>
-        <input />
-        <h2>2. Qual sua idade?</h2>
-        <input />
-        <h2>3. Qual seu email?</h2>
-        <input />
-        <h2>4. Qual a sua escolaridade?</h2>
+
+        <label>1. Qual o seu nome?</label>
+        <input
+          type="text"
+          placeholder={""}
+          value={this.state.value}
+          // onChange={this.}
+        />
+
+        <label>2. Qual sua idade?</label>
+        <input
+          type="text"
+          placeholder={""}
+          value={this.state.value}
+          // onChange={this.}
+        />
+
+        <label>3. Qual seu email?</label>
+        <input
+          type="text"
+          placeholder={""}
+          value={this.state.value}
+          // onChange={this.}
+        />
+
+        <label>4. Qual a sua escolaridade?</label>
         <select>
           <option />
           <option />
           <option />
         </select>
-      </StageContainer>
+      </FirstStageContainer>
     );
   }
 }
--- semana6/labforms/src/components/Etapa3.js
+++ semana6/labforms/src/components/Etapa3.js
@@ -1,8 +1,57 @@
 import React from "react";
 import styled from "styled-components";
 
+const ThirdStageContainer = styled.div`
+  width: 800px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    text-transform: uppercase;
+  }
+
+  label {
+    font-size: x-large;
+    font-weight: 400;
+  }
+
+  input {
+    margin: 20px;
+  }
+
+  select {
+    margin: 20px;
+  }
+`;
+
 export class Etapa3 extends React.Component {
+  state = {
+    whyNotFinished: "",
+    complementaryCourse: "",
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <ThirdStageContainer>
+        <h1>etapa 3 - informações gerais de ensino</h1>
+
+        <label>7. Por que você não terminou um curso de graduação?</label>
+        <input
+          type="text"
+          placeholder={""}
+          value={this.state.value}
+          // onChange={this.}
+        />
+
+        <label>8. Você fez algum curso complementar?</label>
+        <select>
+          <option />
+          <option />
+          <option />
+        </select>
+      </ThirdStageContainer>
+    );
   }
 }
